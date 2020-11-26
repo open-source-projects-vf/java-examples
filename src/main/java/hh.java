9d1650d0b1b85868acaed7eedf3b7f023425e7f4
A. Kuznetsova <71134653+a-kuznetsova@users.noreply.github.com>
2020-11-26 15:12:46 +0100

    Create hh.java

---
+++ src/main/java/hh.java
@@ -0,0 +1,12 @@
+public class hh {
+    
+    public String sameReturnValue(String s) {
+        return s;
+    }
+
+    public void unusedParameter(String s) {
+        if(true) {}
+        System.out.println("Hello world!");
+    }
+    
+}
