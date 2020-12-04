848a7b4353eec2358d0c8b2c8cc117a8c9d4a118
A. Kuznetsova <71134653+a-kuznetsova@users.noreply.github.com>
2020-12-04 16:02:21 +0100

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -13,7 +13,6 @@ public class Test {
 
     public static void main(String[] args) {
         boolean[][] world = gen();
-        show(world);
         System.out.println();
         Scanner s = new Scanner(System.in);
         while (s.nextLine().length() == 0) {
