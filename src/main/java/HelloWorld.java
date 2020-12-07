8f2e4c36c6f1338e0bb2a7192586ad50797dd992
Anastasia Kuznetsova <anastasia.kuznetsova@jetbrains.com>
2020-12-07 18:40:13 +0100

    fixed hello wolrd and test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,19 +3,4 @@ public class HelloWorld {
     public String sameReturnValue(String s) {
         return s;
     }
-
-    public static void main(String[] args){
-        boolean[][] world = gen();
-        show(world);
-        System.out.println();
-        world = nextGen(world);
-        show(world);
-        Scanner s = new Scanner(System.in);
-        while(s.nextLine().length() == 0){
-            System.out.println();
-            world = nextGen(world);
-            show(world);
-            
-        }
-    }
 }
