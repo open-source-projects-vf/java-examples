c1a92e4a6351bae59ab42f91ff3be2c151273902
anastasia.kuznetsova <anastasia.kuznetsova@jetbrains.com>
2021-03-13 17:04:55 +0100

    Update foo

--- src/main/java/higher-order-functions.kt
+++
@@ -1,8 +0,0 @@
-fun calculate(x: Int, y: Int, operation: (Int, Int) -> Int): Int {  // 1
-    return operation(x, y)                                          // 2
-} // 3
-
-fun main() {
-    val mulResult = calculate(4, 5) { a, b -> a * b }               // 5
-    println("mulResult $mulResult")
-}
