2f3a7ddab01218c069d3e9fbd107b23ce773a6d9
A. Kuznetsova <71134653+a-kuznetsova@users.noreply.github.com>
2021-07-16 13:52:30 +0200

    Update Stopwatch.java

--- src/main/java/Stopwatch.java
+++ src/main/java/Stopwatch.java
@@ -7,6 +7,12 @@ public class Stopwatch
     private long startTime;
     private long stopTime;
 	private long unusedVariable;
+    private long dd;
+    private long ds;
+    private long starte3Time;
+    private long stop3Time;
+    private long sta2rtTime;
+    private long sto2rtpTime;
 
     public static final double NANOS_PER_SEC = 1000000000.0;
 
