0cf5c2203e7772e40dc5d28aff3c7c291a9060a7
A. Kuznetsova <71134653+a-kuznetsova@users.noreply.github.com>
2021-10-19 10:29:42 +0200

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,12 @@
 public class HelloWorld {
     
     public String sameReturnValue(String s) {
+        if(true) {
+            System.out.println("Hello");   
+        }
+        if(true) {
+            System.out.println("Hello");   
+        }
         return s;
     }
 }
