9532eb6c4aac7e856a4e9d41f81e357732517110
A. Kuznetsova <71134653+a-kuznetsova@users.noreply.github.com>
2021-12-17 13:22:38 +0100

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
         }
         if(true) {
             System.out.println("Hello");   
+            System.out.println("Hello");   
+            System.out.println("Hello");   
         }
         return s;
     }
